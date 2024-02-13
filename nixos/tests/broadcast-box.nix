0eac208be4cb14ac5c70db1e8234b344c0c2909d
Joshua Manchester <JManch@protonmail.com>
2024-02-13 03:28:10 +0000

    nixos/tests/broadcast-box: init

---
+++ nixos/tests/broadcast-box.nix
@@ -0,0 +1,14 @@
+import ./make-test-python.nix ({ lib, ... }: {
+  name = "broadcast-box";
+  meta.maintainers = with lib.maintainers; [ jmanch ];
+
+  nodes.machine = { ... }: {
+    services.broadcast-box.enable = true;
+  };
+
+  testScript = ''
+    machine.wait_for_unit("broadcast-box.service")
+    machine.wait_for_open_port(8080)
+    machine.succeed("curl --fail http://localhost:8080/")
+  '';
+})
