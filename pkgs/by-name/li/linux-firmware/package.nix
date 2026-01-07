bc10ee274ddf70a9137141faaaa80b4c0001f644
Joshua Manchester <JManch@protonmail.com>
2026-01-07 19:52:50 +0000

    linux-firmware: 2025-12-18 -> 2026-01-06

--- pkgs/by-name/li/linux-firmware/package.nix
+++ pkgs/by-name/li/linux-firmware/package.nix
@@ -22,13 +22,13 @@ let
 in
 stdenvNoCC.mkDerivation {
   pname = "linux-firmware";
-  version = "20251125-unstable-2025-12-18";
+  version = "20251125-unstable-2026-01-06";
 
   src = fetchFromGitLab {
     owner = "kernel-firmware";
     repo = "linux-firmware";
-    rev = "881c549a82203abd9a88870ba27f3e8ce754b2c4";
-    hash = "sha256-ziKzNbP8pqwFilzQb227FtVMqaUGDcbZ57tc9mAMSxs=";
+    rev = "e272e0d1edce58496c3917f7422b9531daeb0535";
+    hash = "sha256-idXFIaw5mSeQe4x+HkNYhOIzQ+R5Bfab7Urer7a/vUs=";
   };
 
   postUnpack = ''
